1f003eb606a862181103085adc27f32e994a4f63
Raphael Forment <raphael.forment@gmail.com>
2022-05-15 21:20:11 +0200

    maybe that

--- public/build/bundle.js
+++ public/build/bundle.js
@@ -9693,7 +9693,7 @@ var app = (function () {
             video: "/videos/Bubo.webm",
             link: "https://raphaelforment.fr",
             description: "Raphaël (bubobubo) est un live-coder actif à Lyon et à Paris. Il est actif au sein du collectif TOPLAP et du mouvement Algorave depuis 2018. Raphaël s'intéresse à l'utilisation du live coding comme technique, instrument et art performatif par son travail de recherche, son enseignement ou ses collaborations avec le collectif parisien Cookie Collective ou avec les lyonnais du LocalHost.",
-            url_portrait: "./photos/lezgo.png",
+            url_portrait: "../photos/lezgo.png",
             id: 1,
         },
         {
@@ -9702,7 +9702,7 @@ var app = (function () {
             video: "/videos/ralt144mi.webm",
             link: "https://rhoumi.github.io/",
             description: "Ralt144MI est une bande sonore de souvenirs vidéos centrée sur un univers aquatique. C'est le résultat de la redécouverte d'un large stock de cassettes vidéos familiales de plongée sous-marine. Le son, codé en direct deviendra support des lectures de bandes, interagissant et évoluant avec elles.",
-            url_portrait: "./photos/ralt144mi.jpeg",
+            url_portrait: "../photos/ralt144mi.jpeg",
             id: 2,
         },
         {
@@ -9711,7 +9711,7 @@ var app = (function () {
             video: "/videos/elie.webm",
             link: "https://linktr.ee/julescipher",
             description: "Frappé assez récemment par la passion du livecoding, Elie Gavoty (Jules Cipher), développe une pratique tournée notamment vers les microrythmes et l'usage de spatialisation pour tenter d'intensifier l'immersion d'une dance music improvisée et atypique.",
-            url_portrait: "./photos/elie.JPG",
+            url_portrait: "../photos/elie.JPG",
             id: 3,
         },
         {
@@ -9719,7 +9719,7 @@ var app = (function () {
             video: "/videos/azertype.webm",
             link: "https://linktr.ee/azertype",
             description: "azertype Half-blood wizard Hardware: XY OS: bipolar & dys Software: anticapitalism & intersectionnality & neurodiversity Powered by Cyberflemme",
-            url_portrait: "./photos/azertype.png",
+            url_portrait: "../photos/azertype.png",
             id: 4,
         },
         {
@@ -9728,7 +9728,7 @@ var app = (function () {
             video: "/videos/leon.mp4",
             link: "https://leon196.github.io/",
             description: "Programmation d'effets visuels improvisées, entre sculptures fractals mathématiques, filtres d'images animés et distorsions de personnages 3D.",
-            url_portrait: "./photos/photo_leon.png",
+            url_portrait: "../photos/photo_leon.png",
             id: 5,
         },
     ];
--- src/information.js
+++ src/information.js
@@ -5,7 +5,7 @@ const performers = [
         video: "/videos/Bubo.webm",
         link: "https://raphaelforment.fr",
         description: "Raphaël (bubobubo) est un live-coder actif à Lyon et à Paris. Il est actif au sein du collectif TOPLAP et du mouvement Algorave depuis 2018. Raphaël s'intéresse à l'utilisation du live coding comme technique, instrument et art performatif par son travail de recherche, son enseignement ou ses collaborations avec le collectif parisien Cookie Collective ou avec les lyonnais du LocalHost.",
-        url_portrait: "./photos/lezgo.png",
+        url_portrait: "../photos/lezgo.png",
         id: 1,
     },
     {
@@ -14,7 +14,7 @@ const performers = [
         video: "/videos/ralt144mi.webm",
         link: "https://rhoumi.github.io/",
         description: "Ralt144MI est une bande sonore de souvenirs vidéos centrée sur un univers aquatique. C'est le résultat de la redécouverte d'un large stock de cassettes vidéos familiales de plongée sous-marine. Le son, codé en direct deviendra support des lectures de bandes, interagissant et évoluant avec elles.",
-        url_portrait: "./photos/ralt144mi.jpeg",
+        url_portrait: "../photos/ralt144mi.jpeg",
         id: 2,
     },
     {
@@ -23,7 +23,7 @@ const performers = [
         video: "/videos/elie.webm",
         link: "https://linktr.ee/julescipher",
         description: "Frappé assez récemment par la passion du livecoding, Elie Gavoty (Jules Cipher), développe une pratique tournée notamment vers les microrythmes et l'usage de spatialisation pour tenter d'intensifier l'immersion d'une dance music improvisée et atypique.",
-        url_portrait: "./photos/elie.JPG",
+        url_portrait: "../photos/elie.JPG",
         id: 3,
     },
     {
@@ -31,7 +31,7 @@ const performers = [
         video: "/videos/azertype.webm",
         link: "https://linktr.ee/azertype",
         description: "azertype Half-blood wizard Hardware: XY OS: bipolar & dys Software: anticapitalism & intersectionnality & neurodiversity Powered by Cyberflemme",
-        url_portrait: "./photos/azertype.png",
+        url_portrait: "../photos/azertype.png",
         id: 4,
     },
     {
@@ -40,7 +40,7 @@ const performers = [
         video: "/videos/leon.mp4",
         link: "https://leon196.github.io/",
         description: "Programmation d'effets visuels improvisées, entre sculptures fractals mathématiques, filtres d'images animés et distorsions de personnages 3D.",
-        url_portrait: "./photos/photo_leon.png",
+        url_portrait: "../photos/photo_leon.png",
         id: 5,
     },
 ];
